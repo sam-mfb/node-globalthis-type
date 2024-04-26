b1f43f0bda54020a39329263b598f0a21071b11f
Sam Davidoff <sam@mfbtech.com>
2024-04-26 13:44:16 +0000

    setup repo

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-import { File } from "formdata-node"
+//@ts-expect-error
+const stream = new globalThis.ReadableStream({
+  //@ts-expect-error
+  start(controller) {
+    controller.enqueue("Hello, ");
+    controller.enqueue("World!");
+    controller.close();
+  },
+});
 
-function main() {
-    const file = new File([], "foo")
+async function readStream() {
+  const reader = stream.getReader();
+  let result;
+  while (!(result = await reader.read()).done) {
+    process.stdout.write(result.value);
+  }
+  console.log();
 }
+
+readStream();
